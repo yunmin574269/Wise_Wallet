f41bc7a68abc29b02d2bf177572957df69df3288
zarcli <zarcli@hotmail.com>
2018-09-04 14:13:53 +0800

    splash

--- src/main.js
+++ src/main.js
@@ -1,12 +1,40 @@
-const electron = require('electron')
+const {app, ipcMain} = require('electron');
+
+const SplashWindow = require('./windows/controllers/splash');
+const SettingWindow = require('./windows/controllers/settings');
+const IndexWindow = require('./windows/controllers/index');
 
 class Main{
     constructor() {
-
+        this.splashWindow = null;
+        this.settingsWindow = null;
+        this.indexWindow = null;
     }
 
     init() {
+        this.initApp();
+    }
 
+    initApp() {
+        app.on('ready', ()=> {
+            this.createSplashWindow();
+        });
+
+        app.on('activate', () => {
+        });
+    };
+
+    createSplashWindow() {
+        this.splashWindow = new SplashWindow();
+        this.splashWindow.show();
+    }
+
+    createSettingsWindow() {
+        this.settingsWindow = new SettingWindow();
+    }
+
+    createIndexWindow() {
+        this.indexWindow = new IndexWindow();
     }
 }
 
