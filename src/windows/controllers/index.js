881295dd732d76038592caabdab84bfb1e4a1796
zarcli <zarcli@hotmail.com>
2018-09-05 21:54:00 +0800

    index

--- src/windows/controllers/index.js
+++ src/windows/controllers/index.js
@@ -1,1 +1,50 @@
 "use strict";
+
+const path = require('path');
+const { BrowserWindow } = require('electron');
+
+//const AppConfig = require('../../configuration');
+
+//const lan = AppConfig.readSettings('language');
+
+//const Common = require('../../common');
+
+class Index {
+  constructor() {
+    this.index = new BrowserWindow({
+      width: 800,
+      height: 600,
+      title: 'Wise Wallet',
+      resizable: true,
+      center: true,
+      show: true,
+      frame: true,
+      autoHideMenuBar: true,
+      alwaysOnTop: false,
+      icon: 'assets/icon.jpg',
+      titleBarStyle: 'hidden',
+    });
+
+    this.index.loadURL(`file://${path.join(__dirname, '/../views/index.html')}`);
+    this.isShown = false;
+  }
+
+  show() {
+    this.index.show();
+    this.isShown = true;
+
+    // 打开开发者工具
+    //this.index.webContents.openDevTools()
+  }
+
+  hide() {
+    this.index.hide();
+    this.isShown = false;
+  }
+
+  close() {
+      this.index.close();
+  }
+}
+
+module.exports = Index;
