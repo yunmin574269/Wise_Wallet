63f044500866ac2ba56cb8ef3c7f527c644e1ea5
zarcli <zarcli@hotmail.com>
2018-09-12 22:22:13 +0800

    sqlite

--- src/windows/lib/sqlite-handle.js
+++ src/windows/lib/sqlite-handle.js
@@ -1,18 +1,19 @@
 "use strict";
 
-var sqlite3 = require('sqlite3');
+var sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
+
+var path = __dirname + "/../../../keystore";
+var fileName = path + "/wallet.dat";
 
 class SqliteHandle {
     constructor() {}
 
     init() {
-        var path = __dirname + "/../../../keystore";
         if(!fs.existsSync(path)) fs.mkdirSync(path);
-        var fileName = path + "/wallet.dat";
         if(!fs.existsSync(fileName)) {
-            var db = new sqlite3.Database(fileName,function() {
-                db.run("create table account_date (" +
+            var db = new sqlite3.Database(fileName,function(err) {
+                db.run("create table account_data (" +
                         "private_key varchar(600) PRIMARY KEY     NOT NULL," +
                         "address varchar(100) NOT NULL," +
                         "name varchar(50) NOT NULL)",function(){
@@ -22,15 +23,48 @@ class SqliteHandle {
         }
     }
 
-    add() {
+    add(private_key, address, name) {
+        var db = new sqlite3.Database(fileName,  function(err){
+            db.run("insert into account_data(" +
+                    "private_key, address, name) " +
+                    "values($key,$address,$name)",
+                    {
+                        $key:private_key,
+                        $address:address,
+                        $name:name
+                    }, function(err){
+                        db.close();
+                    });
+        });
     }
 
-    getOne() {
-
+    getOne(private_key) {
+        var db = new sqlite3.Database(fileName, function(err){
+            db.get("select * from account_data " +
+                    "where private_key=$key", 
+                    {
+                        $key: private_key
+                    },
+                    function(err,row){
+                        console.log(row);
+                        db.close();
+                    });
+        });
     }
 
-    modifyName() {
-        
+    modifyName(private_key, name) {
+        var db = new sqlite3.Database(fileName, function(err){
+            db.run("update account_data " +
+                    "set name=$name " +
+                    "where private_key = $key",
+                    {
+                        $key: private_key,
+                        $name: name
+                    }),
+                    function(err){
+                        this.close();
+                    }
+        });
     }
 }
 
